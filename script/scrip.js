02efa623aeb64828c6144688f516c2b50df5cbcd
Pasha <pashalevchenko1995@gmail.com>
2021-02-28 14:09:49 +0200

    fix

--- script/scrip.js
+++ script/scrip.js
@@ -1,1 +1,15 @@
+let button = document.querySelector('.banner__request_btn')
+let userName = document.querySelector('.banner__input_username')
+button.addEventListener('click', sendRequest)
+
+
+function sendRequest(){
+    let name = userName.value;
+    let email = inputEmail.value;
+    let number = phoneNumber.value;
+    console.log('Имя: ' + name)
+    console.log('Email: ' + email)
+    console.log('Телефон: ' + number)
+    }
+   
 
